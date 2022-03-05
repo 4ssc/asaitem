84940c3f8754be3a3240ce57b8f9aba979fc52cb
4ssc <asai.lefty@gmail.com>
2022-03-05 18:23:56 +0900

    add:4SSC-1 next.jsによるサイト立ち上げ

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+const Home = (): JSX.Element => {
+  return (
+    <div>
+      <main>
+        <h1>
+          Welcome to Next.js!
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home;
